2b9ab415e3d51a49380709fda9324b8f96424215
duy <duynl2@topica.edu.vn>
2018-08-11 13:23:00 +0700

    allow Access-Control-Allow-Origin for heroku domain

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Origin", "https://authenticate-frontend-test.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
